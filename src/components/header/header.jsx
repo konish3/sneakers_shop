5da9b1ef98377bd77b0a4b8a211c927fe8247e0c
Ilya_Konishev <konyshev2002@mail.ru>
2023-08-27 01:50:54 +0300

    creating a header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Header () {
+	return <div className="header">
+		<div className="logo">
+			<img src="" alt="logo" />
+			<h2></h2>
+			<p></p>
+		</div>
+	</div>
+}
