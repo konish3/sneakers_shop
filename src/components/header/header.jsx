0ae0724d2d8f67c59af9e288c5fc640ac9ad3857
Ilya_Konishev <konyshev2002@mail.ru>
2023-08-27 16:36:09 +0300

    ending the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import '../../styles/header.scss'
+import {Link} from 'react-router-dom'
 
 export function Header () {
 	return <div className="header">
-		<div className="logo">
-			<img src="" alt="logo" />
-			<h2></h2>
-			<p></p>
-		</div>
+		<Link to='/'>
+			<div className="logo">
+				<img width={40} height={40} src="img/logo.png" alt="logo" />
+				<div className='header__title'>
+					<h3>Cross New</h3>
+					<p>Магазин лучших кроссовок</p>
+				</div>
+			</div>
+		</Link>
+		<ul className='header__list'>
+			<li className='header__list_basket'>
+				<img src="img/basket.svg" alt="" />
+				<p>1205 руб.</p>
+			</li>
+			<li>
+				<img src="img/hearth.svg" alt="" />
+			</li>
+			<li>
+				<img src="img/User.svg" alt="" />
+			</li>
+		</ul>
 	</div>
 }
